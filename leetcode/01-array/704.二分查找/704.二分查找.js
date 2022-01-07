94bc0b1d499415aa16caa197b817d1e597d6e19e
xiezhifei <xiezhifei@do1.com.cn>
2022-01-07 18:08:42 +0800

    doc：704二分查找

---
+++ leetcode/01-array/704.二分查找/704.二分查找.js
@@ -0,0 +1,64 @@
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+// 方法一 indexOf
+var search = function(nums, target) {
+  return nums.indexOf(target)
+};
+
+// 方法二：遍历
+var search1 = function(nums, target) {
+  let index = -1
+  let len = nums.length
+
+  for (let i = 0; i < len; i++) {
+      if (nums[i] === target) {
+          index = i
+          break
+      }
+  }
+  return index
+};
+
+// 方法三：二分查找，左闭右闭
+
+var search2 = function(nums, target) {
+  let left = 0
+  let right = nums.length - 1
+
+  while (left <= right) {
+    let mid = left + Math.floor((right-left)/2)
+
+    if (nums[mid] < target) {
+      left = mid +1
+      } else if (nums[mid] > target) {
+      right = mid - 1
+    } else {
+      return mid
+    }
+  }
+  return -1
+};
+
+// 方法四：二分查找
+var search4 = function(nums, target) {
+  let left = 0
+  let right = nums.length
+
+  while (left < right) {
+    let mid = left + Math.floor((right-left)/2)
+
+    if (nums[mid] > target) {
+      right = mid 
+      } else if (nums[mid] < target) {
+      left = mid +1
+    } else {
+      return mid
+    }
+  }
+  return -1
+};
